c527beb1654d963c20a23952b093978678fb8b5a
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-18 15:52:37 -0600

    Prevented unnecessary calculation.

--- poly-tester.js
+++ poly-tester.js
@@ -6,11 +6,11 @@ function isPalin(string) {
 // Make all lower case for case insensitivity and replace all spaces, underscores and non-words.
 string = string.toLowerCase().replace(/\s+/g, "").replace(/\W/g,"").replace(/_/g,"");
 console.log(string);
-for(i=0; i<=Math.round(string.length/2); i++) {
+for(i=0; i<=Math.floor(string.length/2-1); i++) {
 	  if(string[i] !== string[string.length-1-i]) {
 	    console.log("Your string is not palindromic!");
 	    break;
-	  } else if(i === Math.round(string.length/2)) {
+	  } else if(i === Math.floor(string.length/2-1)) {
 	    console.log("Your string is palindromic!");
 	  }
 	}

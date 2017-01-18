ae5fd428a46fc60f17d1c040f0d8aef980723df2
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-17 20:44:31 -0600

    Correctected for case sensitivity, underscores etc

--- poly-tester.js
+++ poly-tester.js
@@ -1,8 +1,11 @@
-// Checks if our string is palindromic. Case sensitive. Poop !== pooP.
-var ourString = "poop";
+// Checks if our string is palindromic.
+var ourString = "A Man, /.,.()^&*A Plan, A Canal__-Panama!";
 isPalin(ourString);
 
 function isPalin(string) {
+// Make all lower case for case insensitivity and replace all spaces, underscores and non-words.
+string = string.toLowerCase().replace(/\s+/g, "").replace(/\W/g,"").replace(/_/g,"");
+console.log(string);
 for(i=0; i<=Math.round(string.length/2); i++) {
 	  if(string[i] !== string[string.length-1-i]) {
 	    console.log("Your string is not palindromic!");
